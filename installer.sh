cb0319b806b3a2e23a594ea1d8267d00133d1b7b
Eve LoCastro <locastre@mskcc.org>
2024-05-10 15:32:31 -0400

    full installation complete for CT_cardiac

--- installer.sh
+++ installer.sh
@@ -6,40 +6,47 @@ function print_stratis_logo {
 	echo " " 
 }
 
+N_MODELS=2
+
 function define_models {
 	N=$1
 	case ${N} in
 	   1) 
-		MODELNAME=CT_cardiac_structures_deeplab
-		MODELGIT=H4sIAE2qOWYAA8soKSkottLXT88syShN0kvOz9VPTi0q0ncOiU9OLErJTEyOLy4pKk0uKS1KLY5PSU0tyElM4gIACCFhTTYAAAA=
-		MODELWEIGHTS=H4sIAF6rOWYAAwXBUQ6AIAgA0P8OI3P9VLchQnGmtMDWOn3viftlG0CzShR2fQNpAxO8+QBz9EIgfZlTbCPjitxFlStHf+pIquUkD/mbfnx+XzlIAAAA
+		MODEL_NAME=CT_cardiac_structures_deeplab
+		MODEL_GIT="H4sIAE2qOWYAA8soKSkottLXT88syShN0kvOz9VPTi0q0ncOiU9OLErJTEyOLy4pKk0uKS1KLY5PSU0tyElM4gIACCFhTTYAAAA="
+		MODEL_WEIGHTS="H4sIAD8pPmYAAwXBUQqAIBAFwP8O46YlZLexLTVELN9S0umbSSIXVqKCzKy22hXXQki+HTtBvJxM1Zq5I1o4fY+hvdnrMTwIi5mcRYwqfsMPzAV1z0gAAAA="
+		CONDAPACK="H4sIADABPWYAAwXBUQqAIAwA0P8O46IcSbfRZSoxlm1EePreq2a37gCsF5FL8jkSBq3xyQeoRWsEaSxbiKv3LMgt9ILjnbWeBQXFp+7KmH5thOuWSAAAAA=="
 		;;
 	   2)
-		MODELNAME=CT_Lung_SMIT
-		MODELGIT=""
-		MODELWEIGHTS=""
+		MODEL_NAME=CT_Lung_SMIT
+		MODEL_GIT="NONE"
+		MODEL_WEIGHTS="NONE"
+		CONDAPACK_HASH="NONE"
 	  	;;
-	   3) 
-		MODELNAME=CT_Brain_SMIT
-		MODELGIT=""
-                MODELWEIGHTS=""
-	  	;;
-	   4)
-		MODELNAME=CT_HeadAndNeck_SelfAttention
-		MODELGIT=""
-                MODELWEIGHTS=""
-		;;
+#	   3) 
+#		MODEL_NAME=CT_Brain_SMIT
+#		MODEL_GIT="NONE"
+#               MODEL_WEIGHTS="NONE"
+#		CONDAPACK_HASH="NONE"
+#	  	;;
+#	   4)
+#		MODEL_NAME=CT_HeadAndNeck_SelfAttention
+#		MODEL_GIT="NONE"
+#               MODEL_WEIGHTS="NONE"
+#		CONDAPACK_HASH="NONE"
+#		;;
 	   *)
 		echo Error
 		;;
 	esac
 
-	echo $MODELNAME `base64 -d <<<${MODELGIT} | gunzip` `base64 -d <<<${MODELWEIGHTS}`
+	echo $MODEL_NAME ${MODEL_GIT} ${MODEL_WEIGHTS} ${CONDAPACK}
 }
 
 function print_model_opts {
+	N_MODELS=2
         echo "The following are the list of available models. When passing the argument to installer, select the number of the model to download: "
-	for N in `seq 1 4`
+	for N in `seq 1 ${N_MODELS}`
 	do
 		echo "          	${N}.  `define_models ${N} | awk '{ print $1 }'`"
 	done
@@ -49,9 +56,9 @@ function help_text {
 	echo "Usage Information: "
 	echo "	Flags: "
 	echo "		-i : Flag to run installer in interactive mode (no argument)"
-	echo "		-m : [1-4] Integer number to select model to install. For list of available options, see below. "
+	echo "		-m : [1-2] Integer number to select model to install. For list of available options, see below. "
 	echo "		-d : Directory to install model with network weights "
-	echo "		-p : [Y/N] Setup and install Anaconda environment to run model from environment.yml. User must already have Anaconda installed and initiated. "
+	echo "		-p : [P/C/N] Setup and install Python environment P:Conda env from YAML; C: Conda pack download; N: No install. " #User must already have Anaconda installed and initiated. "
 	echo "		-h : Print help menu "
 	echo " "
 	print_model_opts
@@ -69,9 +76,10 @@ intro_text
 
 #Initialize default options
 IMODE=0
-MODELTARG=""
+MODEL_NUM=1
+MODEL_NAME="NONE"
 INSTALLDIR="$HOME"
-PYSETUP="N"
+POPTION="N"
 
 
 # parse input arguments
@@ -85,15 +93,16 @@ while getopts ":hm:d:ip:" opt; do
         MODEL_NUM=${OPTARG}
 	MODEL_STRING=`define_models ${MODEL_NUM}`
 	MODEL_NAME=`echo ${MODEL_STRING} | awk '{ print $1 }'`
-	if [ "${MODELNAME}" == "Error" ]; then MODELTARG=""; fi
+	if [ "${MODEL_NAME}" == "Error" ]; then MODEL_NUM="NONE"; fi
       ;;
     d)
         INSTALLDIR=${OPTARG}
       ;;
     p)
-	PYSETUP=${OPTARG}
-	if [[ "${PYSETUP}" != "Y" && "${PYSETUP}" != "N" ]]; then PYSETUP=N; fi
+	POPTION=${OPTARG}
+	if [[ "${POPTION}" != "P" && "${POPTION}" != "C" && "${POPTION}" != "N" ]]; then POPTION="N"; echo "Selected -p Python option invalid, defaulting to N"; fi
       ;;
+
     i)
 	IMODE=1
       ;;
@@ -117,6 +126,9 @@ then
 	echo "======================================="
 
 	echo " " 
+	echo "Press Ctrl-C at any time to exit"
+	echo " " 
+
 	echo "Step 1. Model Selection"
 	echo "***********************"
 	echo " " 
@@ -129,7 +141,11 @@ then
 
 	if [ "${USR_ANS}" != "" ]; then MODEL_NUM=${USR_ANS}; fi
 
-	MODEL_NAME=`define_models ${MODEL_NUM} | awk '{ print $1 }'`
+	MODEL_STRING=`define_models ${MODEL_NUM}`
+	MODEL_NAME=`echo ${MODEL_STRING} | awk '{ print $1 }'`
+	MODEL_GIT_HASH=`echo ${MODEL_STRING} | awk '{ print $2 }'`
+	MODEL_WEIGHTS_HASH=`echo ${MODEL_STRING} | awk '{ print $3 }'`
+	CONDAPACK_HASH=`echo ${MODEL_STRING} | awk '{ print $4 }'`
 
 	if [ "${MODEL_NAME}" != "Error" ]
 	then 
@@ -158,21 +174,86 @@ then
 	echo "Step 3. Python Option"
 	echo "*********************"
 	echo " " 
-	echo "Please indicate if the installer should create the Conda environment [${PYSETUP}]: "
+	echo "Please select the option indicate if the installer should create the Python environment [${POPTION}]: " # [${PYSETUP}]: "
+	echo "	P : Set up Conda environment from included YAML file"
+	echo "  C : Download Conda-Pack environment"
+	echo "  N : No Python setup"
 
 	read USR_ANS
 	
 	if [ "${USR_ANS}" != "" ]
 	then
-		if [ "${PYSETUP}" != "Y" && "${PYSETUP}" != "N" ]; then PYSETUP=N; fi
+		if [[ "${USR_ANS}" != "P" &&  ${USR_ANS} != "C" && "${USR_ANS}" != "N" ]]; then POPTION=N; else POPTION=${USR_ANS}; fi
 	fi
 
-	echo "Conda env setup option selected is ${PYSETUP}"
+	echo "Python setup option selected is ${POPTION}"
+
 
 	echo "Proceeding with installation and setup"
 	
+else
+	MODEL_STRING=`define_models ${MODEL_NUM}`
+        MODEL_NAME=`echo ${MODEL_STRING} | awk '{ print $1 }'`
+        MODEL_GIT_HASH=`echo ${MODEL_STRING} | awk '{ print $2 }'`
+        MODEL_WEIGHTS_HASH=`echo ${MODEL_STRING} | awk '{ print $3 }'`
+        CONDAPACK_HASH=`echo ${MODEL_STRING} | awk '{ print $4 }'`
 fi
 
 # Verify all selected options
 
 # Commence with install
+
+# Option 1: Install cardiac substructures DeepLab
+if [ "${MODEL_NUM}" == "1" ]
+then
+	cd ${INSTALLDIR}
+	MODEL_GIT=`base64 -d <<<${MODEL_GIT_HASH} | gunzip`
+	echo git clone ${MODEL_GIT}
+	git clone ${MODEL_GIT}
+
+	MODEL_FOLDER=`basename ${MODEL_GIT} | sed "s/.git//g"`
+	MODEL_PATH=${INSTALLDIR}/${MODEL_FOLDER}
+	echo cd ${MODEL_PATH}
+	cd ${MODEL_PATH}
+
+	MODEL_WEIGHTS=`base64 -d <<<${MODEL_WEIGHTS_HASH} | gunzip`
+	echo wget -O model_weights.tar.gz -L ${MODEL_WEIGHTS}
+	wget -O model_weights.tar.gz -L ${MODEL_WEIGHTS}	
+	echo tar xf model_weights.tar.gz	
+	tar xf model_weights.tar.gz
+
+	if [ "${POPTION}" == "C" ]
+	then 
+		#download conda-pack
+		mkdir ${MODEL_PATH}/conda-pack
+		cd ${MODEL_PATH}/conda-pack
+		CONDAPACK=`base64 -d <<<${CONDAPACK_HASH} | gunzip`
+		echo wget -O condapack.tar.gz -L ${CONDAPACK}
+		wget -O condapack.tar.gz -L ${CONDAPACK}
+		echo tar xf condapack.tar.gz
+		tar xf condapack.tar.gz
+	elif [ "${POPTION}" == "P" ]
+	then
+		#set up Conda environment
+                CONDAHOME=`conda info | grep "base environment" | awk '{ print $4 }'`
+                if [ -z "${CONDAHOME}" ]
+                then
+                        echo "Anaconda may not be installed; python setup cannot continue"
+			echo "Exiting."
+                        exit 1
+                fi
+		conda create -y --name ${MODEL_NAME} python=3.8
+
+		source ${CONDAHOME}/etc/profile.d/conda.sh
+
+		conda activate ${MODEL_NAME}
+		pip install -r requirements.txt
+	fi
+fi
+
+
+# Option 2: Install CT_Lung_SMIT
+#if [ "${MODEL_NUM}" == "2"] 
+#then
+#
+#fi
